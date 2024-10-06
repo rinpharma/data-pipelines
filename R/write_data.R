b9f45b2ea20ae4c4a295c000d4448363e69674a5
James Black <james@epijim.uk>
2024-10-06 22:03:39 +0200

    switch to parquet

--- R/write_data.R
+++ R/write_data.R
@@ -6,21 +6,19 @@ write_data <- function(
 
 
   if (!dir.exists("output")) dir.create("output")
+  
+  # function so no mismatch
+  help_save_file <- function(data){
+    # Capture the name of the argument
+    name_of_input <- as.character(substitute(data))
 
-  readr::write_rds(
-    all_data <- list(
-      raw_data = raw_data,
-      processed_team = processed_team,
-      processed_proceedings = processed_proceedings
-    ),
-    "output/all_data.rds"
-  )
+    # write it
+    output_loc <- glue::glue("output/{name_of_input}.parquet")
+    message(glue::glue("writing {output_loc}"))
+    nanoparquet::write_parquet(data,output_loc)
+  }
 
-  readr::write_csv2(
-    processed_team, "output/processed_team.csv"
-  )
-
-  readr::write_csv2(
-    processed_proceedings, "output/processed_proceedings.csv"
-  )
+  help_save_file(processed_team)
+  help_save_file(processed_proceedings)
+  help_save_file(processed_workshops)
 }
