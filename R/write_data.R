24353b6550fbddda2fb7ef343e2fac89ccf98918
Harvey Lieberman <harvey.lieberman@gmail.com>
2025-09-29 21:00:13 -0400

    write out talks as csv

--- R/write_data.R
+++ R/write_data.R
@@ -18,9 +18,22 @@ write_data <- function(
     message(glue::glue("writing {output_loc}"))
     nanoparquet::write_parquet(data,output_loc)
   }
+  
+  help_save_csv <- function(data){
+    # Capture the name of the argument
+    name_of_input <- as.character(substitute(data))
+
+    # write it
+    output_loc <- glue::glue("output/{name_of_input}.csv")
+    message(glue::glue("writing CSV {output_loc}"))
+    write.csv(data,output_loc, row.names = FALSE)
+  }
 
   help_save_file(processed_team)
   help_save_file(processed_proceedings)
   help_save_file(processed_talks)
   help_save_file(processed_workshops)
+
+  help_save_csv(processed_talks)
+
 }
